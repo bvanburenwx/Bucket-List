4bd8ea25798f56cca0cf5841b5ecfc912246de8f
BVanBuren <bvanburenwx@gmail.com>
2021-10-16 12:33:28 -0400

    fixed bugs that were not letting the page create

--- src/components/Bucket.js
+++ src/components/Bucket.js
@@ -11,9 +11,8 @@ function Bucket(props) {
   console.log(props.bucket);
 
   const submitUpdate = (value) => {
-
+    props.editBucketItem(edit.id, value);
     // TODO: Write logic to update the `edit` value in state after a user updates an entry in the list
-    setEdit(value);
     // TODO: Set the key:value pairs in the `edit` object back to empty strings
     setEdit({
       id: null,
@@ -31,15 +30,15 @@ function Bucket(props) {
     // TODO: Add a className of `bucket row complete ${item.eagerness}` for completed items, and `bucket-row ${item.eagerness}` for non-completed items
     // TODO: Add a key attribute set to the value of the index position
     // Hint: use a ternary operator
-    <div className={item === 'complete' ? `bucket-row complete ${item.eagerness}` : `bucket-row ${item.eagerness}` } key={ index }>
+    <div className={item.isComplete ? `bucket-row complete ${item.eagerness}` : `bucket-row ${item.eagerness}` } key={ index }>
 
-      // TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument
-      <div key={ index } onClick={ () => props.completeBucketItem(item.id)}>
+      {/*TODO: Add an onClick event that invokes the `completeBucketItem` method passing the item id as a argument*/}
+      <div key={item.id} onClick={() => props.completeBucketItem(item.id)}>
           {/* TODO: Add the item text here */}
-          {item.value}
+          {item.text}
       </div>
       <div className="icons">
-        // TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties
+        {/* TODO: Add an onClick event update the `edit` object with the `id`, `value`, and `eagerness` properties*/}
         <p onClick={() => submitUpdate(item.id, item.value, item.eagerness)}> ✏️</p>
         {/* TODO: Add an onClick event that will invoke the removeBucketItem method passing in the `item.id` */}
         <p onClick={() => props.removeBucketItem(item.id) }> 🗑️</p>
--- src/components/BucketList.js
+++ src/components/BucketList.js
@@ -7,7 +7,7 @@ function BucketList() {
 
   // Function to add a bucket list item
   const addBucketItem = (item) => {
-    setBucket([...setBucket, item]);
+    setBucket([...bucket, item]);
     // TODO: Write logic to add the new bucket item to the bucket state variable
     
   };
@@ -18,7 +18,10 @@ function BucketList() {
     let updatedBucket = bucket.map((item) => {
       
       // TODO: Write logic that marks an item as complete or incomplete when invoked
-      item.id === id && item === 'complete';
+      if (item.id === id) {
+        item.isComplete = !item.isComplete;
+      }
+      return item
     });
 
     setBucket(updatedBucket);
@@ -27,9 +30,9 @@ function BucketList() {
   // Function to remove bucket list item and update state
   const removeBucketItem = (id) => {
     // TODO: Write logic that will return an array of items that don't contain the ID passed to this function
-    let bucketsLeft = bucket.filter(item => {
-      item.id !== id
-    })
+    let bucketsLeft = bucket.filter((item) => 
+      item.id !== id);
+    
 
     // TODO: Update the bucket state variable
     setBucket(bucketsLeft);
